80160e8c22f142c8f8ef0c0cee32d0f03a35dd70
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-09-19 21:59:46 +0530

    Fix logTimestamp to use Date() instead of toISOString() for logging format

--- Express Practice/logTimestamp.js
+++ Express Practice/logTimestamp.js
@@ -1,5 +1,6 @@
 const logTimestamp = (req, res, next) => {
-  const now = new Date().toISOString();
+  //const now =new Date().toISOString();
+  const now = Date();
   console.log(`[${now}] Request to: ${req.method} ${req.url}`);
   next();
 };
