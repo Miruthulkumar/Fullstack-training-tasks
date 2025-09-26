541d5ad41ab7da0b500c94042c7f86ec0bac2c00
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-09-26 16:27:13 +0530

    -

--- Js practice/LongestWordinSentence.js
+++ Js practice/LongestWordinSentence.js
@@ -1,8 +1,6 @@
 const longestWord = (sentence) => {
   let words = sentence.split(" ");
-
   let longestWord = words[0];
-
   for (let i = 0; i < words.length; i++) {
     if (words[i].length > longestWord.length) {
       longestWord = words[i];
