b65b36bf59903d0662d43ec376ffb2692a7a1649
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-10-04 21:46:09 +0530

    hemlet for safety

--- Express Practice/server.js
+++ Express Practice/server.js
@@ -3,6 +3,7 @@ import express from "express"; //can be used after adding the type:"module" in p
 import logTimestamp from "./middleware/logTimestamp.js";
 import generalLimiter from "./middleware/rateLimiter.js";
 import responseTime from "response-time";
+import helmet from "helmet";
 const app = express();
 
 //import env from dotenv
@@ -15,6 +16,8 @@ connectDb();
 
 //middleware to use json
 app.use(express.json());
+
+app.use(helmet());
 
 //calling timestamp middleware
 app.use(logTimestamp);
