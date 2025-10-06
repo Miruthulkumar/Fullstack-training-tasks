cdef0578f9c1047b067d838a2683f0a5803303db
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-10-06 22:14:39 +0530

    fix: update document title and clean up App component formatting

--- User Management System/React Frontend/user-management-system-fe/src/App.jsx
+++ User Management System/React Frontend/user-management-system-fe/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+
 
 function App() {
 
+  
 
   return (
     <>
-      
-      
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
