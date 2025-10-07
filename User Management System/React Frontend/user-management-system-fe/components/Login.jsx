cac13635e4e7e57975d9eaca2658f4e91da90f84
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-10-07 16:34:10 +0530

    feat: implement Login component and update favicon

---
+++ User Management System/React Frontend/user-management-system-fe/components/Login.jsx
@@ -0,0 +1,26 @@
+const Login = () => {
+
+
+return (
+    <div>
+
+                    <div className="loginbgdiv">
+                        <h1>Login</h1>
+                        <div className="logindivs">
+                                <p style={{ margin: 0, marginRight: '8px' }}>User id</p>
+                                <input className="inputfield" type="text" placeholder="Enter your userid" />
+                        </div>
+                        <div className="logindivs">
+                                <p style={{ margin: 0, marginRight: '8px' }}>Password</p>
+                                <input className="inputfield" type="password" placeholder="Enter your Password" />
+                        </div>
+                        <div>
+                            <button className="button">Login</button>
+                        </div>
+                    </div>
+
+    </div>
+);
+};
+
+export default Login;
