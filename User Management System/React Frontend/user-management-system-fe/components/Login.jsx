1b1d5c8f7dd9362e1efd6dbf1d5c6348ed5cafe7
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-10-07 16:36:14 +0530

    refactor: improve formatting and indentation in Login component

--- User Management System/React Frontend/user-management-system-fe/components/Login.jsx
+++ User Management System/React Frontend/user-management-system-fe/components/Login.jsx
@@ -1,26 +1,30 @@
 const Login = () => {
-
-
-return (
+  return (
     <div>
-
-                    <div className="loginbgdiv">
-                        <h1>Login</h1>
-                        <div className="logindivs">
-                                <p style={{ margin: 0, marginRight: '8px' }}>User id</p>
-                                <input className="inputfield" type="text" placeholder="Enter your userid" />
-                        </div>
-                        <div className="logindivs">
-                                <p style={{ margin: 0, marginRight: '8px' }}>Password</p>
-                                <input className="inputfield" type="password" placeholder="Enter your Password" />
-                        </div>
-                        <div>
-                            <button className="button">Login</button>
-                        </div>
-                    </div>
-
+      <div className="loginbgdiv">
+        <h1>Login</h1>
+        <div className="logindivs">
+          <p style={{ margin: 0, marginRight: "8px" }}>User id</p>
+          <input
+            className="inputfield"
+            type="text"
+            placeholder="Enter your userid"
+          />
+        </div>
+        <div className="logindivs">
+          <p style={{ margin: 0, marginRight: "8px" }}>Password</p>
+          <input
+            className="inputfield"
+            type="password"
+            placeholder="Enter your Password"
+          />
+        </div>
+        <div>
+          <button className="button">Login</button>
+        </div>
+      </div>
     </div>
-);
+  );
 };
 
 export default Login;
